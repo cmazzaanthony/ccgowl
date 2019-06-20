f7b6d6d9316aaa6406df996bdd9401d041ecde86
Cody Mazza-Anthony <cmazzaanthony@gmail.com>
2019-06-20 09:48:55 -0400

    Updated setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,10 @@ setup(
     name='src',
     packages=find_packages(),
     version='0.1.0',
-    description='A short description of the project.',
+    description='ccgowl is a free software package for structure based on the Python programming language. '
+                'It can be used with the interactive Python interpreter or executing Python scripts. Its main '
+                'purpose is to allow for experimenting with state-of-the-art methods for learning structure in '
+                'precision matrix estimation. This package includes implementations of GRAB, GOWL, and ccGOWL models.',
     author='Cody Mazza-Anthony',
     license='MIT',
 )
